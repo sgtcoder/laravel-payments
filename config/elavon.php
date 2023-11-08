5a758a5573cc284eac78348ffe53611fc675510f
coder <usmccoder@gmail.com>
2023-11-08 08:17:54 -0700

    base config files

---
+++ config/elavon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'merchant_id' => env('ELAVON_MERCHANT_ID'),
+    'username' => env('ELAVON_USERNAME'),
+    'password' => env('ELAVON_PASSWORD'),
+];
---
+++ config/payeezy.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'api_key' => env('PAYEEZY_API_KEY'),
+	'api_secret' => env('PAYEEZY_API_SECRET'),
+	'merchant_token' => env('PAYEEZY_MERCHANT_TOKEN'),
+];
