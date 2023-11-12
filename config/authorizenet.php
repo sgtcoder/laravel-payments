9a61457f7dfb411ebbdc9610d339ec3ef45b1578
coder <usmccoder@gmail.com>
2023-11-12 10:45:43 -0700

    add config for authnet

---
+++ config/authorizenet.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'login_id' => env('AUTHORIZE_NET_LOGIN_ID'),
+    'client_key' => env('AUTHORIZE_NET_CLIENT_KEY'),
+    'transaction_key' => env('AUTHORIZE_NET_TRANSACTION_KEY')
+];
